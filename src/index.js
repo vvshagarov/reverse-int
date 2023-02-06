960f606209314e6ff1b27c6c702d7f21f1c95925
vvshagarov <shagarov-vv@mail.ru>
2023-02-06 20:55:31 +0400

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function reverse (n) {
-  
+    var strnum = String(n);
+    revnum = '';
+    var i = String(n).length;
+    if (n >= 0) {
+        while (i > 0) {
+            revnum = revnum + strnum[i - 1];
+            i--;
+        }
+    return +revnum;
+    }
+    else {
+        while (i > 1) {
+            revnum = revnum + strnum[i - 1];
+            i--;
+        }
+    return +revnum;
+    }
 }
